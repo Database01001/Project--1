a6f3b8ee3a3164e3429675de1d2d36282717229a
Divyanshu Shukla <75030391+DivyanshuShukla001@users.noreply.github.com>
2020-11-26 18:31:06 +0530

    Create CODES.cpp

---
+++ CODES.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+
+int id;
+id = 01;
+
+int password;
+password = 88823;
+
+int id1 = 01;
+
+int id2 = 02;
+
+int password2;
+password2 = 88824;
+
+
+cout << "KiD KoDeR" <<endl;
+cout << "Enter Your id: ";
+cin >> id1;
+cout << "Enter Password: ";
+
+if(id1 != 01);{
+
+cout << " Name : xyz "<<endl;
+cout << "Connection-specific DNS suffix  . : 10.110.11100.111110000.1100" <<endl;
+
+/*Wireless LAN adapter Wi-Fi:
+
+   Connection-specific DNS Suffix  . :
+   Link-local IPv6 Address . . . . . : fe80::f416:7510:37f5:5c63%6
+   IPv4 Address. . . . . . . . . . . : 192.168.43.198
+   Subnet Mask . . . . . . . . . . . : 255.255.255.0
+   Default Gateway . . . . . . . . . : 192.168.43.182
+
+password=.{53ED43A9-572C-4E80-949D-8B3DAFA43458}:
+
+   Media State . . . . . . . . . . . : Media connected
+   Connection-specific DNS Suffix  . : --Null--
+
+Tunnel adapter Teredo Tunneling Pseudo-Interface:
+
+   Connection-specific DNS Suffix  . :
+   IPv6 Address. . . . . . . . . . . : 2001:0:348b:fb58:1cf9:9f7:d0e0:7a73
+   Link-local IPv6 Address . . . . . : fe80::1cf9:9f7:d0e0:7a73%16
+   Default Gateway . . . . . . . . . : ::
+
+Tunnel adapter isatap.{E5EAB77C-D9B6-4D73-83BC-A04BE6D98E83}:
+
+   Media State . . . . . . . . . . . : Media disconnected
+   Connection-specific DNS Suffix  . :" << endl;
+*/
+}
+if(id1 != 01) {
+
+cout <<"Invalid User ID Enter again:";
+cin >> id1;
+
+cout << " Name . . . . . . . . . . . : xyz;" << endl;
+cout << " Connection-specific DNS Suffix  . : 10.110.11100.111110000.1100;" << endl;
+/*
+Wireless LAN adapter Wi-Fi:
+
+   Connection-specific DNS Suffix  . :
+   Link-local IPv6 Address . . . . . : fe80::f416:7510:37f5:5c63%6
+   IPv4 Address. . . . . . . . . . . : 192.168.43.198
+   Subnet Mask . . . . . . . . . . . : 255.255.255.0
+   Default Gateway . . . . . . . . . : 192.168.43.182
+
+password=.{53ED43A9-572C-4E80-949D-8B3DAFA43458}:
+
+   Media State . . . . . . . . . . . : Media connected
+   Connection-specific DNS Suffix  . : --Null--
+
+Tunnel adapter Teredo Tunneling Pseudo-Interface:
+
+   Connection-specific DNS Suffix  . :
+   IPv6 Address. . . . . . . . . . . : 2001:0:348b:fb58:1cf9:9f7:d0e0:7a73
+   Link-local IPv6 Address . . . . . : fe80::1cf9:9f7:d0e0:7a73%16
+   Default Gateway . . . . . . . . . : ::
+
+Tunnel adapter isatap.{E5EAB77C-D9B6-4D73-83BC-A04BE6D98E83}:
+
+   Media State . . . . . . . . . . . : Media disconnected
+   Connection-specific DNS Suffix  . :" << endl;
+*/
+}
+
+cout << "Enter Password to access in:";
+cin >>password;
+
+if(password != 88823 );{
+
+cout << "!!You got access!!" <<endl;
+cout << "Download Your new ID and password:";
+cout << "Here" <<endl;
+cin >> id2;
+cout << "Now Enter Password:";
+cin >> password2;
+}
+if(id2 = 03){
+
+cout <<"Hurray!!" <<endl;
+
+}
+
+return 0;
+
+}
